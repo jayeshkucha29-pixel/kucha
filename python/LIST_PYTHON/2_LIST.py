3e87f26c9f166c3f6fb5ca0622b5e16d512fd029
jayeshkucha29-pixel <jayeshkucha29@gmail.com>
2026-01-07 15:32:06 +0530

    Add files via upload

---
+++ python/LIST_PYTHON/2_LIST.py
@@ -0,0 +1,19 @@
+#create a list & store 10 different type of values in it 
+
+list =['The Easy Learn',32,12.4,False,True,44,55,66.6,4,"India"]
+
+#print all values 
+print(list)
+
+#print 1st value 
+print(list[0])
+
+#print 1st value 
+print(list[0:5])
+
+#  print list twice using * 
+print(list*2)
+
+#print item in the list from 2nd to 5th position
+print(list[0:6])
+      
