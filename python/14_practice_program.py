470f62ec4fd86a05b0e94f728f90e2bab587222e
jayeshkucha29-pixel <jayeshkucha29@gmail.com>
2026-01-19 14:36:17 +0530

    Add files via upload

--- python/14_practice_program.py
+++ python/14_practice_program.py
@@ -11,6 +11,7 @@ print("sum of numbers",total)
 
 numbers = [1,2,3,4,5,6,7]
 
+
 maximum = max(numbers)
 minimum = min(numbers)
 
@@ -20,3 +21,46 @@ print("minimum number of",minimum)
 
 
 #Create a tuple of 5 elements and find its length.
+
+tuple=(11,22,33,44,55,66,77,88,0.3)
+
+length = len(tuple)
+
+print("tuple:",tuple)
+print("length of tuple :",length)
+
+#Reverse a list using slicing (no loop).
+
+list = (11,22,33,44,55)
+
+reversed_list = list[::-1]
+
+print("original list :",list)
+print("reveresed list",reversed_list)
+
+#Count how many times a specific value appears in a list using built-in methods.
+
+list = [2,3,4,2,5,6,2,3]
+
+count = list.count(6)
+
+print(count)
+
+# calendar
+
+import calendar
+
+year = int(input("enter the year: "))
+month = int(input("enter the month: "))
+
+cal = calendar.monthcalendar(year,month)
+
+print("mon,tu,we,th,fr,sa,su")
+for week in cal:
+    for day in week:
+        if day == 0:
+            print(" ", end=" ")
+        else:
+            print(f"{day:2}", end=" ")
+        print()
+        
